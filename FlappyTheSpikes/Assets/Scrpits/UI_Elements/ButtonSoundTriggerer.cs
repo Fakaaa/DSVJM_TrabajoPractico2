3ffbded8808d7015fc4068c3ae69172db3a27c33
FacundoPonce <mrfacu125@gmail.com>
2021-11-26 07:44:51 -0300

    Added sounds to the game.

---
+++ FlappyTheSpikes/Assets/Scrpits/UI_Elements/ButtonSoundTriggerer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using AudioManagerScript;
+
+public class ButtonSoundTriggerer : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+{
+    [SerializeField] public string soundHover;
+    [SerializeField] public string soundClick;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        AudioManager.Instance.Play(soundClick);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        AudioManager.Instance.Play(soundHover);
+    }
+
+}
